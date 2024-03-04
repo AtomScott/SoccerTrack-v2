a537b6d9eb61ebdf3e60216ab7e9a5cadf067382
AtomScott <atom.james.scott@gmail.com>
2024-03-04 11:58:54 +0900

    train in parallel

--- sh_scripts/detection/train_val.sh
+++ sh_scripts/detection/train_val.sh
@@ -1,8 +1,25 @@
-python scripts/yolov8/train_val.py \
-  --input ./datasets/roboflow-split/2024-02-28-trainsize10-randomstate1/data.yaml \
-  --output ./outputs/det_metrics_dict_2024-02-28-trainsize10-randomstate1.json \
-  --epochs 50 \
-  --imgsz 640 \
-  --batch 16 \
-  --workers 8 \
-  --device cuda:0 
+#!/bin/bash
+
+# Define the base directory for datasets and outputs
+dataset_base="./datasets/roboflow-split"
+output_base="./outputs/change_trainset_size"
+
+# Loop over all directories in the roboflow-split directory
+for dir in ${dataset_base}/*; do
+  # Check if the directory contains a data.yaml file
+  if [[ -f "${dir}/data.yaml" ]]; then
+    # Extract the folder name for naming the output file
+    folder_name=$(basename "${dir}")
+    
+    # Run the training/validation script with the found dataset
+    qsub /groups/gaa50073/atom/soccertrack-v2/sh_scripts/abci/run_AG_small.sh \
+      python scripts/yolov8/train_val.py \
+      --input "${dir}/data.yaml" \
+      --output "${output_base}/${folder_name}.json" \
+      --epochs 50 \
+      --imgsz 2160 \
+      --batch 4 \
+      --workers 8 \
+      --device cuda:0
+  fi
+done
