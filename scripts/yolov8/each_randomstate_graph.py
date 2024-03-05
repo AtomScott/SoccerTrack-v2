633bc833ff064d5bec8cb9fc4fad0573abf8287d
KurodaKento0505 <kuroda.kento@image.iit.tsukuba.ac.jp>
2024-03-05 19:25:46 +0900

    In the process of making error bar graph

--- scripts/yolov8/each_randomstate_graph.py
+++ scripts/yolov8/each_randomstate_graph.py
@@ -13,12 +13,13 @@ def parse_args():
         Namespace: The arguments namespace.
     """
     parser = argparse.ArgumentParser(description='Save the graph on a specific path.')
-    parser.add_argument('--input_10', type=str, required=True, help='Input dataset_10.json file for making graphs') # ...\outputs\results.json\dataset_10.json (file path)
-    parser.add_argument('--input_50', type=str, required=True, help='Input dataset_50.json file for making graphs') # ...\outputs\results.json\dataset_50.json (file path)
-    parser.add_argument('--input_100', type=str, required=True, help='Input dataset_100.json file for making graphs') # ...\outputs\results.json\dataset_100.json (file path)
-    parser.add_argument('--output_all_directory', type=str, required=True, help='Output directory for saving all class graphs') # ...\outputs\graph\all_class (directory path)
-    parser.add_argument('--output_person_directory', type=str, required=True, help='Output directory for saving person class graphs') # outputs\graph\person_class (directory path)
-    parser.add_argument('--output_ball_directory', type=str, required=True, help='Output directory for saving ball class graphs') # outputs\graph\ball_class (directory path)
+    parser.add_argument('--input_10', type=str, required=True, help='Input dataset_10.json file for making graphs') # ...\outputs\change_trainset_size\randomstate1\dataset_10.json (file path)
+    parser.add_argument('--input_50', type=str, required=True, help='Input dataset_50.json file for making graphs') # ...\outputs\change_trainset_size\randomstate1\dataset_50.json (file path)
+    parser.add_argument('--input_100', type=str, required=True, help='Input dataset_100.json file for making graphs') # ...\outputs\change_trainset_size\randomstate1\dataset_100.json (file path)
+    parser.add_argument('--output_all_directory', type=str, required=True, help='Output directory for saving all class graphs') # ...\outputs\graph\randomstate1\all_class (directory path)
+    parser.add_argument('--output_person_directory', type=str, required=True, help='Output directory for saving person class graphs') # outputs\graph\randomstate1\person_class (directory path)
+    parser.add_argument('--output_ball_directory', type=str, required=True, help='Output directory for saving ball class graphs') # outputs\graph\randomstate1\ball_class (directory path)
+    parser.add_argument('--output_np', type=str, required=True, help='Output json file for saving all results') # outputs\graph\randomstate1\all_results_np (file path)
     return parser.parse_args()
 
 
@@ -87,6 +88,15 @@ def main():
         plt.plot(x, y, color ="red")
         file_path = os.path.join(args.output_ball_directory, str(index_name_list[i]) + ".png")
         plt.savefig(file_path)
+
+    # make np file including all results to make error bar graph
+    all_results_np = np.full([3,3,4], 1.0)
+    all_results_np[0] = all_class_np
+    all_results_np[1] = person_class_np
+    all_results_np[2] = ball_class_np
+
+    np.save(args.output_np, all_results_np)
+    print(all_results_np)
 
 
 if __name__ == "__main__":
---
+++ scripts/yolov8/error_bar_graph.py
@@ -0,0 +1,81 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+import os
+
+
+def parse_args():
+    """
+    Parse command line arguments.
+
+    Returns:
+        Namespace: The arguments namespace.
+    """
+    parser = argparse.ArgumentParser(description='Save the graph on a specific path.')
+    parser.add_argument('--input_randomstate1', type=str, required=True, help='Input all_results_np1.npy file for making a error bar graph') # ...\outputs\graph\randomstate1\all_results_np1.npy (file path)
+    parser.add_argument('--input_randomstate2', type=str, required=True, help='Input all_results_np2.npy file for making a error bar graph') # ...\outputs\graph\randomstate1\all_results_np2.npy (file path)
+    parser.add_argument('--input_randomstate3', type=str, required=True, help='Input all_results_np3.npy file for making a error bar graph') # ...\outputs\graph\randomstate1\all_results_np3.npy (file path)
+    parser.add_argument('--input_randomstate4', type=str, required=True, help='Input all_results_np4.npy file for making a error bar graph') # ...\outputs\graph\randomstate1\all_results_np4.npy (file path)
+    parser.add_argument('--input_randomstate5', type=str, required=True, help='Input all_results_np5.npy file for making a error bar graph') # ...\outputs\graph\randomstate1\all_results_np5.npy (file path)
+    parser.add_argument('--output_all_directory', type=str, required=True, help='Output directory for saving all class graphs') # outputs\graph\randomstate1\all_class (directory path)
+    parser.add_argument('--output_person_directory', type=str, required=True, help='Output directory for saving person class graphs') # outputs\graph\randomstate1\person_class (directory path)
+    parser.add_argument('--output_ball_directory', type=str, required=True, help='Output directory for saving ball class graphs') # outputs\graph\randomstate1\ball_class (directory path)
+    return parser.parse_args()
+
+
+def main():
+
+    args = parse_args()
+
+    # load a npy file of each randomstate
+    for i in range(1,6):
+        exec("np_open = args.input_randomstate" + str(i))
+        exec("randomstate_np" + str(i) + "= np.load(eval('np_open'))")
+        print(i)
+        exec("print(randomstate_np" + str(i) + ")")
+        print("\n")
+
+
+    # calculate means of each indexes of each class
+    
+    # name list of validation indexes
+    index_name_list = ['Precision', 'Recall', 'mAP50', 'mAP50-95']
+
+    # mean_np have means of each validation index of each trainset of each class
+    mean_np = np.full((3,3,4), 0.0)
+    # sum_np have sums of each validation index of each trainset of each class
+    sum_np = np.full((3,3,4), 0.0)
+    # sd_np have standard deviations of each validation index of each trainset of each class
+    sd_np = np.full((3,3,4), 1.0)
+    # value_np have values of each validation index of each trainset of each class
+    value_np = np.full(5, 1.0)
+
+
+    for i in range(4):
+        for j in range(3):
+            for k in range(3):
+                for l in range(1,6):
+                    exec("sum_np[k,j,i] += randomstate_np" + str(l) + "[k,j,i]")
+                    exec("value_np[l] += randomstate_np" + str(l) + "[k,j,i]")
+    mean_np = sum_np / 5
+    print(sum_np, '\n')
+    print(mean_np)
+
+
+    # make error bar graphs
+    for k in range(3):
+        for i in range(4):
+            
+            x = [10, 50, 100]
+            y = [mean_np[k,j,i] for j in range(3)]
+            plt.figure()
+            plt.title(index_name_list[i]) 
+            plt.xlabel("train dataset") 
+            plt.ylabel(index_name_list[i]) 
+            plt.plot(x, y, color ="red")
+            file_path = os.path.join(args.output_person_directory, str(index_name_list[i]) + ".png")
+            plt.savefig(file_path)
+
+
+if __name__ == "__main__":
+    main()
